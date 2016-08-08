d4699c25724a8ae7d18a9f07062b501dfd41df3c
heleneshaikh <helene.shaikh@gmail.com>
2016-08-08 14:21:23 +0200

    add functional test

--- app/src/test/java/com/udacity/gradle/builditbigger/JokesTaskTest.java
+++ app/src/test/java/com/udacity/gradle/builditbigger/JokesTaskTest.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by heleneshaikh on 06/08/16.
